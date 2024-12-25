d72e6d17bc74ff6db7dd0cb233ba198a9c4f9baa
abusou7 <112393861+abusou7@users.noreply.github.com>
2024-12-25 22:18:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,31 @@ links.forEach(link => {
         document.getElementById(target).classList.add('active');
     });
 });
+
+emailjs.init('yURl2NI4q_au1DHvE'); // استبدل YOUR_USER_ID بمعرف المستخدم الخاص بك من EmailJS
+
+const form = document.querySelector('#apply form');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const data = {
+        to_name: '707 Squad',
+        from_name: document.getElementById('fullname').value,
+        message: `
+            العمر: ${document.getElementById('age').value}
+            الخبرات السابقة: ${document.getElementById('experience').value}
+            المهارات: ${document.getElementById('skills').value}
+            سبب التقديم: ${document.getElementById('reason').value}
+        `
+    };
+
+    emailjs.send('blackpanther707squad', '707squad', data)
+        .then(() => {
+            alert('تم إرسال الطلب بنجاح!');
+            form.reset(); // إعادة تعيين الحقول
+        })
+        .catch(err => {
+            console.error('حدث خطأ أثناء الإرسال:', err);
+            alert('حدث خطأ أثناء الإرسال. يرجى المحاولة لاحقًا.');
+        });
+});
